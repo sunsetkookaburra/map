dc9599371a3874e5752169c17eae4f625f814d04
Oliver Lenehan <oli@sunsetkookaburra.xyz>
2023-01-20 12:34:07 +1100

    fix: touch misc

--- controls.js
+++ controls.js
@@ -38,24 +38,41 @@ L.Control.Drawing = L.Control.extend({
     point.href = "#point";
     point.title = "Point";
     point.textContent = "📍";
+    const line = L.DomUtil.create("a", "skmaps-control-line", menu);
+    line.href = "#point";
+    line.title = "Point";
+    line.textContent = "✏️";
 
     // responsible for resetting to null
     let onCancel = null;
     let state = null;
 
-    function escCancel(event) {
+    this._escCancel = function escCancel(event) {
       if (event.originalEvent.key == "Escape") onCancel?.(state);
     }
 
     L.DomEvent.disableClickPropagation(menu);
-    map.on("keydown", escCancel);
+    map.on("keydown", this._escCancel);
 
+    L.DomEvent.on(menu, "click", ev => ev.preventDefault());
+    L.DomEvent.on(menu, "touchstart", ev => ev.preventDefault());
     L.DomEvent.on(menu, "pointerdown", ev => {
+      ev.preventDefault();
+      ev.target.releasePointerCapture(ev.pointerId);
+    });
+    L.DomEvent.on(menu, "pointerup", ev => {
+      ev.preventDefault()
       // cancel current
+      const oldCancel = onCancel;
       onCancel?.(state);
       // activate new
-      // ev.target.releasePointerCapture(ev.pointerId);
-      if (ev.target == point) pointStart(ev.pointerType != "mouse");
+      ev.target.releasePointerCapture(ev.pointerId);
+      if (ev.target == point) {
+        if (oldCancel !== pointCancel) pointStart(ev.pointerType != "mouse");
+      }
+      else if (ev.target == line) {
+        if (oldCancel !== lineCancel) lineStart(ev.pointerType != "mouse");
+      }
     });
 
     // // L.DomEvent.disableClickPropagation(home);
@@ -103,108 +120,43 @@ L.Control.Drawing = L.Control.extend({
       point.classList.remove("skmaps-control-active");
     }
 
-    // function lineStart(event) {
+    // function lineStart(touch = false) {
+    //   // Point initial placement dependent on input method
+    //   // Touch: Place centre to be moved later
+    //   // Mouse: Move cursor and click to place
+    //   const origin = touch ? map.getCenter() : MOUSE;
+    //   // Common point
+    //   state = { node: L.nodeMarker(origin, { interactive: false }).addTo(map) };
 
-    // }
-
-    // function lineUpdate(state) {
-
-    // }
-
-    // function lineCancel(state) {
-
-    // }
-
-    // function lineComplete(state) {
-
-    // }
-
-    // function cancelPoint() {
-    //   map.off("mousemove", moveState);
-    //   point.classList.remove("skmaps-control-active");
-    //   state.remove();
-    //   state = null;
-    // }
-
-    // function moveState() {
-    //   state.setLatLng(MOUSE);
-    // }
-
-    // L.DomEvent.disableClickPropagation(point);
-    // L.DomEvent.on(point, "click touchend", function (ev) {
-    //   ev.preventDefault();
-
-    //   cancelState?.();
-    //   if (cancelState === cancelPoint) {
-    //     cancelState = null;
+    //   if (touch) {
+    //     pointComplete()
     //     return;
     //   }
 
-    //   if (ev instanceof TouchEvent) {
-    //     L.marker(map.getCenter(), {
-    //       draggable: true,
-    //       autoPan: true,
-    //     }).bindPopup((marker) => {
-    //       const { lat, lng } = marker.getLatLng();
-    //       const div = L.DomUtil.create("div");
-    //       div.insertAdjacentHTML("afterbegin", `<a href="?lat=${roundPlaces(lat, 6)}&lon=${roundPlaces(lng, 6)}"><span style="user-select:none">Lat: </span>${roundPlaces(lat, 6)},<br/><span style="user-select:none">Lon: </span>${roundPlaces(lng, 6)}</a><br/><br/>`);
-    //       const a = L.DomUtil.create("a", "", div);
-    //       a.href = "#remove";
-    //       a.textContent = "Remove";
-    //       L.DomEvent.on(a, "click", function (ev) {
-    //         ev.preventDefault();
-    //         L.DomEvent.off(a);
-    //         marker.remove();
-    //       });
-    //       return div;
-    //     }).addTo(map);
-    //     return;
-    //   }
-
-    //   cancelState = cancelPoint;
-
+    //   state.node.options.interactive = false;
     //   point.classList.add("skmaps-control-active");
-    //   state = L.marker(MOUSE, {
-    //     draggable: true,
-    //     autoPan: true,
-    //   });
-    //   map.on("mousemove", moveState);
-    //   function plop() {
-    //     map.off("mousemove", moveState);
-    //     map.off("click", plop);
-    //     point.classList.remove("skmaps-control-active");
-    //     cancelState = null;
-    //     state = null;
-    //   }
-    //   map.on("click", plop);
-    //   state.addTo(map);
+    //   onCancel = lineCancel;
+    //   map.on("click", lineComplete);
+    //   map.on("mousemove", lineUpdate);
+    // }
 
+    // function lineUpdate() {
 
-    //   state.bindPopup((marker) => {
-    //     const { lat, lng } = marker.getLatLng();
-    //     const div = L.DomUtil.create("div");
-    //     div.insertAdjacentHTML("afterbegin", `<a href="?lat=${roundPlaces(lat, 6)}&lon=${roundPlaces(lng, 6)}"><span style="user-select:none">Lat: </span>${roundPlaces(lat, 6)},<br/><span style="user-select:none">Lon: </span>${roundPlaces(lng, 6)}</a><br/><br/>`);
-    //     const a = L.DomUtil.create("a", "", div);
-    //     a.href = "#remove";
-    //     a.textContent = "Remove";
-    //     L.DomEvent.on(a, "click", function (ev) {
-        //   ev.preventDefault();
-        //   L.DomEvent.off(a);
-        //   marker.remove();
-        // });
-        // return div;
-      // });
-    // });
-    // const line = L.DomUtil.create("a", "skmaps-control-line", menu);
-    // line.href = "#";
-    // line.title = "Line";
-    // line.textContent = "✏️";
+    // }
+
+    // function lineCancel() {
+
+    // }
+
+    // function lineComplete() {
+
+    // }
+
     return menu;
   },
   onRemove: function (map) {
-    for (const child of this.getContainer()) {
-      L.DomEvent.off(child);
-    }
+    L.DomEvent.off(this.getContainer());
+    map.off("keydown", this._escCancel);
   }
 });
 

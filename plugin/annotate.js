9a58f3ce0a72cdf0faabda0c8597bb434be8f6cf
Oliver Lenehan <oli@sunsetkookaburra.xyz>
2023-03-29 19:08:04 +1100

    refactor: drawing mvc

--- plugin/annotate.js
+++ plugin/annotate.js
@@ -1,19 +1,7 @@
-/*
-
-Start:
-+ register mouse listener
-
-Cancel:
-+ cleanup state
-
-Complete:
-+ successful end state
-
-*/
 
 document.head.insertAdjacentHTML(
   "beforeend",
-  `<style>
+`<style>
   .skmaps-control {
     font-size: 18px;
     text-shadow: 0px 2px 3px black;
@@ -32,14 +20,6 @@ L.Marker.Pin = L.Marker.extend({
   }
 });
 
-L.Marker.Node = L.Marker.extend({
-  options: {
-    icon: L.icon({ iconUrl: `node-small.svg`, iconSize: L.point(10, 10) }),
-    draggable: true,
-    autoPan: true,
-  }
-})
-
 L.marker.pin = function (latlng, opts) {
   return new L.Marker.Pin(latlng, opts).bindPopup(marker => {
     const { lat, lng } = marker.getLatLng();
@@ -57,172 +37,152 @@ L.marker.pin = function (latlng, opts) {
   });
 }
 
+L.Marker.Node = L.Marker.extend({
+  options: {
+    icon: L.icon({ iconUrl: `node-small.svg`, iconSize: L.point(10, 10) }),
+    draggable: true,
+    autoPan: true,
+    bubblingMouseEvents: true,
+  }
+})
+
 L.marker.node = function (latlng, opts) {
   return new L.Marker.Node(latlng, opts);
 }
 
-/* Leaflet Annotation State Machine Class */
-
 L.Annotate = L.Class.extend({
-  options: {},
-  initialize: function (opts) {
-    L.Util.setOptions(this, opts);
+  annotateCreate: function (ev) {
+    new this.constructor.Model().addTo(ev.map);
+    ev.onFinish();
   },
-  annotateStart: function (map, finish, { touch, latlng }) {
-    finish();
-  },
-  annotateEnd: function (map) {},
-  getContainer: function () {
-    return this._container;
-  },
-  _container: null,
-});
-
-L.Annotate.Pin = L.Annotate.extend({
-  annotateStart: function (map, finish, { touch, latlng }) {
-    // Point initial placement dependent on input method
-    // Touch: Place centre to be moved later
-    // Mouse: Move cursor and click to place
-    const marker = L.marker.pin(latlng, { interactive: false }).addTo(map);
-
-    this._state = { marker, finish };
-
-    if (touch) {
-      this._complete(map.getCenter());
-      return;
-    }
-
-    this._state.marker.options.interactive = false;
-    map.on("click", this._click, this);
-    map.on("mousemove", this._move, this);
-  },
-  annotateEnd: function (map) {
-    map.off("click", this._click, this);
-    map.off("mousemove", this._move, this);
-    this._state.marker.remove();
-    this._state = null;
-  },
-  _move: function (event) {
-    this._state.marker.setLatLng(event.latlng);
-  },
-  _complete: function (latlng) {
-    this._state.finish([L.marker.pin(latlng)]);
-  },
-  _click: function (event) {
-    this._complete(event.latlng);
-  }
-});
-
-
-/*
-
-TODO:
-
-Quirky node placement
-don't rely on map double click for end
-use click on final to end.
-
-*/
-
-L.Annotate.Polyline = L.Annotate.extend({
-  annotateStart: function (map, finish, { touch, latlng }) {
-    this._state = { finish, layer: L.featureGroup(), map };
-    this._state.layer.addTo(map);
-    this._state.line = L.polyline([]).addTo(this._state.layer);
-    map.doubleClickZoom.disable();
-    map.getContainer().classList.add("leaflet-crosshair");
-    map.on("click", this._click, this);
-    map.on("mousemove", this._mousemove, this);
-  },
-  annotateEnd: function (map) {
-    map.off("click", this._click, this);
-    map.off("mousemove", this._mousemove, this);
-    map.getContainer().classList.remove("leaflet-crosshair");
-    // dblclick fires after this for some weird reason (after mouseup probably)
-    // add a mouseup that waits for a done flag, then deletes itself
-    setTimeout(() => { map.doubleClickZoom.enable() }, 50);
-    const polyline = this;
-    this._state.line.bindPopup(function (layer) {
-      return `Length: ${polyline.getLength().toFixed(1)}m`;
-    });
-  },
-  getLatLngs: function () {
-    const latlngs = [];
-    for (const layer of this._state.layer.getLayers()) {
-      if (layer instanceof L.Marker) {
-        latlngs.push(layer.getLatLng());
-      }
-    }
-    return latlngs;
-  },
-  getLength: function () {
-    const points = this.getLatLngs();
-    let length = 0;
-    for (let i = 1; i < points.length; ++i) {
-      length += points[i - 1].distanceTo(points[i]);
-    }
-    return length;
-  },
-  _click: function (event) {
-    if (this.getLatLngs().length > 0) {
-      const lastPoint = map.latLngToContainerPoint(this.getLatLngs().slice(-1)[0]);
-      const distPixels = lastPoint.distanceTo(event.containerPoint);
-      if (distPixels < 5) {
-        this._state.line.setLatLngs(this._state.line.getLatLngs().slice(0, -1));
-        this._state.finish();
-        return;
-      }
-    }
-    const node = L.marker.node(event.latlng);
-    this._lastNode = node;
-    this._state.layer.addLayer(node);
-    this._state.line.addLatLng(event.latlng);
-    if (this._state.line.getLatLngs().length == 1) {
-      this._state.line.addLatLng(event.latlng);
-    }
-    node.on("drag", function (ev) {
-      this._state.line.setLatLngs(this.getLatLngs());
-      this._state.line.closePopup();
-    }, this);
-  },
-  _mousemove: function (event) {
-    this._state.line.setLatLngs([...this.getLatLngs(), event.latlng]);
-  },
-  _lastNode: null,
+  annotateMove: function (model, ev) {},
+  annotateResume: function (model, ev) {},
+  annotateCancel: function (model) {},
+  annotateEnd: function (model, ev) {},
 });
 
 L.annotate = {};
 
-L.annotate.pin = function (opts) {
-  return new L.Annotate.Pin(opts);
+// Controller
+L.Annotate.Pin = L.Annotate.extend({
+  annotateCreate: function(ev) {
+    const latlng = ev.touch ? ev.map.getCenter() : ev.latlng;
+    const model = L.marker.pin(latlng).addTo(ev.map);
+    if (ev.touch) ev.onFinish();
+    model.dragging.disable();
+    return model;
+  },
+  annotateResume: function (model, ev) {
+    ev.onFinish();
+    model.dragging.enable();
+  },
+  annotateMove: function (model, ev) {
+    model.setLatLng(ev.latlng);
+  },
+  annotateCancel: function (model, ev) {
+    model.remove();
+  },
+});
+
+L.Annotate.Polyline = L.Annotate.extend({
+  annotateCreate: function (ev) {
+    ev.map.getContainer().classList.add("leaflet-crosshair");
+    const model = L.featureGroup([L.layerGroup(), L.polyline([])]).addTo(ev.map);
+    return model;
+  },
+  annotateResume: function (model, ev) {
+    const nodes = model.getLayers()[0];
+    const nodesLayers = nodes.getLayers();
+    const polyline = model.getLayers()[1];
+    const oldtail = this._getNode(nodes, -1);
+    oldtail?.off("click");
+    const newtail = L.marker.node(ev.latlng);
+    nodes.addLayer(newtail);
+    polyline.addLatLng(ev.latlng);
+    newtail.on("drag", dragev => {
+      if (model.isPopupOpen()) model.openPopup(dragev.latlng);
+      const latlngs = [];
+      for (const node of nodes.getLayers()) {
+        latlngs.push(node.getLatLng());
+      }
+      polyline.setLatLngs(latlngs);
+    });
+    newtail.on("click", clickev => {
+      ev.map.doubleClickZoom.disable();
+      newtail.off("click");
+      ev.onFinish();
+      model.bindPopup(layer => {
+        let dist = 0;
+        const latlngs = polyline.getLatLngs();
+        for (let i = 1; i < latlngs.length; ++i) {
+          dist += ev.map.distance(latlngs[i-1], latlngs[i]);
+        }
+        return `Length: ${dist.toFixed(0)}m`
+      });
+      ev.map.getContainer().classList.remove("leaflet-crosshair");
+      setTimeout(() => { ev.map.doubleClickZoom.enable(); }, 50);
+    });
+  },
+  annotateCancel: function (model, ev) {
+    ev.map.getContainer().classList.remove("leaflet-crosshair");
+    model.getLayers()[1].getLatLngs().pop();
+    model.getLayers()[1].redraw();
+  },
+  annotateMove: function (model, ev) {
+    const polyline = model.getLayers()[1];
+    const latlngs = polyline.getLatLngs();
+    latlngs.pop();
+    polyline.addLatLng(ev.latlng);
+  },
+  _getNode: function (model, idx) {
+    const layers = model.getLayers();
+    idx %= layers.length;
+    if (idx < 0) idx = layers.length - idx;
+    return layers[idx];
+  },
+});
+
+L.annotate.pin = function () {
+  return new L.Annotate.Pin();
 }
 
-L.annotate.polyline = function (opts) {
-  return new L.Annotate.Polyline(opts);
+L.annotate.polyline = function () {
+  return new L.Annotate.Polyline();
 }
 
 /* Leaflet Annotation Control  */
 
 L.Control.Annotate = L.Control.extend({
+  _currentController: null,
+  _currentModel: null,
+  /** @type {Map<L.Layer, HTMLElement> | Map<HTMLElement, L.Layer>} */
+  _controllerContainersMap: null,
   options: {
     position: "topleft",
   },
   initialize: function (widgets, opts) {
     L.Control.prototype.initialize.call(this, opts);
-    this._widgets = widgets;
+    this._widgetsList = widgets;
   },
   /** Setup leaflet control bar */
   onAdd: function (map) {
+    this._map = map;
+    const attr = "<a style='position:relative' href='https://github.com/sunsetkookaburra/maps'><img src='./res/github-mark.svg' width='10' height='10' style='position:relative;object-fit:contain;height:1em;top:0.1em'> SkAnnotate</a>";
+    map.attributionControl.addAttribution(attr);
     const menu = L.DomUtil.create("div", "leaflet-bar leaflet-control");
-    const widgets = new Map();
-    for (const label in this._widgets) {
-      const a = L.DomUtil.create("a", "skmaps-control", menu);
-      a.href = "#";
-      a.textContent = label;
-      widgets.set(a, this._widgets[label]);
-      this._widgets[label]._container = a;
+
+    // Map of element<->controller map
+    this._controllerContainersMap = new Map();
+    for (const [label, controller] of Object.entries(this._widgetsList)) {
+      const container = L.DomUtil.create("a", "skmaps-control", menu);
+      container.href = "#";
+      container.textContent = label;
+
+      this._controllerContainersMap.set(controller, container);
+      this._controllerContainersMap.set(container, controller);
     }
-    map.on("keydown", this._escCancel, this);
+    this._onkeydown = this._onkeydown.bind(this);
+    window.addEventListener("keydown", this._onkeydown);
     L.DomEvent.disableClickPropagation(menu);
     L.DomEvent.on(menu, "click", ev => ev.preventDefault());
     L.DomEvent.on(menu, "touchstart", ev => ev.preventDefault());
@@ -232,48 +192,68 @@ L.Control.Annotate = L.Control.extend({
     });
     L.DomEvent.on(menu, "pointerup", ev => {
       ev.preventDefault();
-      const oldContainer = this._currentTool?.getContainer();
-      this._cancel();
-      // activate new
       ev.target.releasePointerCapture(ev.pointerId);
 
-      if (ev.target === oldContainer || ev.target === menu) {
+      // Radio Button Behaviour
+      if (this._radioPop("cancel") === ev.target || ev.target == menu) {
         return;
       }
 
-      this._currentTool = widgets.get(ev.target);
-      this._currentTool.getContainer().classList.add("skmaps-control-active");
-      this._currentTool.annotateStart(map, (function finish(layers) {
-        if (layers instanceof Array) {
-          for (const layer of layers) layer.addTo(map);
-        } else if (layers instanceof L.Layer) {
-          layers.addTo(map);
-        }
-        this._cancel();
-      }).bind(this), { touch: ev.pointerType != "mouse", latlng: this._latlng });
+      const container = ev.target;
+      const controller = this._currentController = this._controllerContainersMap.get(ev.target);
+      container.classList.add("skmaps-control-active");
+      this._currentModel = controller.annotateCreate({
+        map,
+        onFinish: () => { this._radioPop(); },
+        touch: ev.pointerType != "mouse",
+        latlng: this._latlng
+      });
     }, this);
-    map.on("mousemove", this._mouseUpdate, this);
+    map.on("click", this._onclick, this);
+    map.on("mousemove", this._mousemove, this);
     return menu;
   },
   /** remove control bar */
   onRemove: function (map) {
     L.DomEvent.off(this.getContainer());
-    map.off("keydown", this._escCancel, this);
-    map.off("mousemove", this._mouseUpdate, this);
+    window.removeEventListener("keydown", this._onkeydown);
+    map.off("mousemove", this._mousemove, this);
+    map.off("click", this._onclick, this);
   },
-  _escCancel: function (event) {
-    if (event.originalEvent.key == "Escape") {
-      this._cancel();
+  _onkeydown: function (event) {
+    if (event.key == "Escape") {
+      this._radioPop("cancel");
     }
   },
-  _cancel: function () {
-    this._currentTool?.annotateEnd(map);
-    this._currentTool?.getContainer().classList.remove("skmaps-control-active");
-    this._currentTool = null;
+  _onclick: function (ev) {
+    this._currentController?.annotateResume(
+      this._currentModel,
+      Object.assign(ev, {
+        onFinish: () => { this._radioPop(); },
+        map: this._map,
+      }),
+    );
   },
-  _currentTool: null,
-  _mouseUpdate: function (ev) {
+  _radioPop: function (doAnnotateCancel = "") {
+    const controller = this._currentController;
+    this._currentController = null;
+    if (controller !== null) {
+      if (doAnnotateCancel === "cancel") controller.annotateCancel(this._currentModel, {
+        map: this._map, latlng: this._latlng
+      });
+      const container = this._controllerContainersMap.get(controller);
+      container.classList.remove("skmaps-control-active");
+      return container;
+    } else {
+      return null;
+    }
+  },
+  _mousemove: function (ev) {
     this._latlng = ev.latlng;
+    this._currentController?.annotateMove(
+      this._currentModel,
+      Object.assign(ev, { onFinish: () => { this._radioPop(); } }),
+    );
   },
 });
 
